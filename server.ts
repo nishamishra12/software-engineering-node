fcb49096029d8775cc760e84cf4485dd07196410
nishamishra12 <nishamishra093@gmail.com>
2022-02-20 21:59:37 -0500

    Revert "Assignment 2"
    This reverts commit bb65233562183f99851b3fb968fad2775a77f3cd.

--- server.ts
+++ server.ts
@@ -1,51 +1,28 @@
-/**
- * @file Implements an Express Node HTTP server. Declares RESTful Web services
- * enabling CRUD operations on the following resources:
- * <ul>
- *     <li>users</li>
- *     <li>tuits</li>
- *     <li>likes</li>
- * </ul>
- * 
- * Connects to a remote MongoDB instance hosted on the Atlas cloud database
- * service
- */
-import express, {Request, Response} from 'express';
-import CourseController from "./controllers/CourseController";
+import express from 'express';
 import UserController from "./controllers/UserController";
+import UserDao from "./daos/UserDao";
+import bodyParser from "body-parser";
 import TuitController from "./controllers/TuitController";
-import LikeController from "./controllers/LikeController";
+import TuitDao from "./daos/TuitDao";
 import mongoose from "mongoose";
-
-// build the connection string
-const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = "nisha_am";
-const DB_PASSWORD = "passwordprotected";
-const HOST = "cluster0.odz5c.mongodb.net";
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-// connect to the database
-mongoose.connect(connectionString);
-
 const app = express();
-app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
+const url ="mongodb+srv://nisha_am:passwordprotected@cluster0.odz5c.mongodb.net" +
+    "/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(url);
 
-// create RESTful Web service API
-const courseController = new CourseController(app);
-const userController = UserController.getInstance(app);
-const tuitController = TuitController.getInstance(app);
-const likesController = LikeController.getInstance(app);
+app.get('/hello', (req, res) =>
+    res.send('Hello World!'));
 
-/**
- * Start a server listening at port 4000 locally
- * but use environment variable PORT on Heroku if available.
- */
+app.get('/add/:a/:b', (req, res) => {
+    res.send(req.params.a + req.params.b);
+})
+
+const userController = new UserController(app, new UserDao());
+const tuitController = new TuitController(app, new TuitDao());
+
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
