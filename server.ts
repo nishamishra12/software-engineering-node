da270b0a3ddf72b197851416484493f43d3d6723
nishamishra12 <nishamishra093@gmail.com>
2022-04-02 01:41:08 -0400

    initial commit

--- server.ts
+++ server.ts
@@ -1,28 +1,77 @@
-import express from 'express';
+/**
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
+ * <ul>
+ *     <li>users</li>
+ *     <li>tuits</li>
+ *     <li>likes</li>
+ * </ul>
+ * 
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
+ */
+import express, {Request, Response} from 'express';
+import CourseController from "./controllers/CourseController";
 import UserController from "./controllers/UserController";
-import UserDao from "./daos/UserDao";
-import bodyParser from "body-parser";
 import TuitController from "./controllers/TuitController";
-import TuitDao from "./daos/TuitDao";
+import LikeController from "./controllers/LikeController";
+import SessionController from "./controllers/SessionController";
+import AuthenticationController from "./controllers/AuthenticationController";
 import mongoose from "mongoose";
+import GroupController from "./controllers/GroupController";
+const cors = require("cors");
+const session = require("express-session");
+
+// build the connection string
+const PROTOCOL = "mongodb+srv";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const HOST = "cluster0.m8jeh.mongodb.net";
+const DB_NAME = "myFirstDatabase";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+mongoose.connect(connectionString);
+
 const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN
+}));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+let sess = {
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === "production",
+    }
+}
 
-const url ="mongodb+srv://nisha_am:passwordprotected@cluster0.odz5c.mongodb.net" +
-    "/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(url);
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+}
 
-app.get('/hello', (req, res) =>
-    res.send('Hello World!'));
+app.use(session(sess))
+app.use(express.json());
 
-app.get('/add/:a/:b', (req, res) => {
-    res.send(req.params.a + req.params.b);
-})
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
 
-const userController = new UserController(app, new UserDao());
-const tuitController = new TuitController(app, new TuitDao());
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
 
+// create RESTful Web service API
+const courseController = new CourseController(app);
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+const likesController = LikeController.getInstance(app);
+SessionController(app);
+AuthenticationController(app);
+GroupController(app);
+/**
+ * Start a server listening at port 4000 locally
+ * but use environment variable PORT on Heroku if available.
+ */
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
